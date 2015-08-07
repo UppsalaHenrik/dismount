822cc47e022d1e0a3a370e83aad42d2fbe885259
UppsalaHenrik <henrik.b.nyberg@farmbio.uu.se>
2015-08-07 14:53:32 +0100

    More documentation

--- R/getUserSlurmQ.R
+++ R/getUserSlurmQ.R
@@ -1,12 +1,18 @@
+#' Get SLURM queue for active user
+#'
+#' Gets the slurm queue for the user that is currently logged in.
+#' No options available. Returns a character string with the rows
+#' of the queue, including the header.
+#'
 
 getUserSlurmQ <- function(){
-  
+
   # Get user name
   userName <- system('echo "$USER"', intern=TRUE)
-  
+
   # Get the slurm queue for the active user
   slurmQ <- system(paste("squeue", "-u", userName), intern=TRUE)
-  
+
   return(slurmQ)
 }
 
