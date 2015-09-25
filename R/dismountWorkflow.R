a7e122cc8dab2f44bed1eed90c7206c1f3634f57
UppsalaHenrik <henrik.b.nyberg@farmbio.uu.se>
2015-09-25 16:40:30 +0200

    More edits to workflow function

--- R/dismountWorkflow.R
+++ R/dismountWorkflow.R
@@ -4,6 +4,10 @@
 #'
 #' @param modFileName The model file to use. Default is "run83.mod".
 #' @param retries The number of retries to run as part of the initial parallel retries.
+#' @param doParaRetries Whether or not the parallel retries should be run. Default is TRUE. If FALSE then a rerunDirName is expected.
+#' @param doPrecond Whether or not preconditioning should be run on all samples. Default is FALSE
+#' @param doDismount Whether or not dismount should be run on all samples. Default is TRUE.
+#' @param rerunDirName If doParaRetries is FALSE, then this directory will be assumed to contain a parallel retries run with model files and outputs to use for dismount and/or precond. No default.  
 #'
 #' @author Henrik Bjugård Nyberg - henrik.b.nyberg@@farmbio.uu.se
 #'
@@ -49,14 +53,52 @@ dismountWorkflow <- function(modFileName, retries = 9, doParaRetries = TRUE,
   retry <- rawresNoNA$model - 1
   paraRetriesRawresNoNA <- cbind(retry, paraRetriesRawresNoNA)
   
-  # List the Retry model files for dismount runs
+  # List the Retry files for dismount and/or precond runs
+  # This might need to be more strict
   retryModFilePaths <- list.files(path = paraRetriesDirName, pattern = "retry.+mod$")
+  retryFilePaths <- list.files(path = paraRetriesDirName, pattern = "retry.+$")
   
-  if()
+  if(doPrecond){
+    
+    # Copy those files into the precond runs directory
+    file.copy(paste0(paraRetriesDirName, "/", retryFilePaths), precondRunsDir)
+    
+    # Set the precond runs directory as WD
+    setwd(precondRunsDir)
+    
+    # Run precond on the models
+    precondDirList <- sapply(retryModFilePaths, runPrecond)
+    # I've had issues with the runs not strting before I start the wait below, so here is a little initial wait
+    Sys.sleep(10)
+    
+    # Wait for the queue to have only the master job left
+    waitForSlurmQ(targetLength = 1)
+    
+    # Find and parse the rawres files, and then put them together
+    precondRawresFiles <- list.files(recursive = TRUE)[grep("pert_init_est_modelfit/raw_results.csv", list.files(recursive = TRUE))]
+    precondRawresList <- lapply(precondRawresFiles, parseRawres)
+    precondRawres <- do.call("rbind", precondRawresList)
+    
+    # I bind in the retry number as well
+    # This is a little dangerous as it assumes the order is the same...
+    # I could do this within the apply above instead, and parse the actual number...
+    precondRawres <- cbind(retry[-1], precondRawres)
+    
+    # Pick out the
+    overMinOfvPrecondRetries <- subset(precondRawres, ofv > minOfv + 1)
+    nOverMinOfvPrecondRetries <- nrow(overMinOfvPrecondRetries)
+    
+    # Print a message about the number of retries over
+    print(paste("After precond on", length(retryModFilePaths), "samples,",
+                nOverMinOfvPrecondRetries, "samples were over minimum OFV by 1 or more"))
+    
+  }
+  
+  setwd(workflowWD)
   
   if(doDismount){  
     # Copy those files into the dismount runs directory
-    file.copy(paste0(paraRetriesDirName, "/", retryModFilePaths), dismountRunsDir)
+    file.copy(paste0(paraRetriesDirName, "/", retryFilePaths), dismountRunsDir)
     
     # Set the dismount runs directory as WD
     setwd(dismountRunsDir)
