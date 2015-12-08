45daa9d4114af9a1e856322b8c7393492dfc1c64
UppsalaHenrik <henrik.b.nyberg@farmbio.uu.se>
2015-12-08 16:01:28 +0000

    Better options handling for plotSurface calling createRawresInput

--- R/plotSurface.R
+++ R/plotSurface.R
@@ -12,12 +12,12 @@
 
 
 plotSurface <- function(plotlyAccount, modFilePath, paramsToCompare = c("THETA1", "THETA2"), 
-                        xLims, yLims, resol = 10, local = FALSE){
+                        resol = 10, local = FALSE, ...){
   
   require(plotly)
   
   print("Creating the rawres input file")
-  rawresInputList <- createRawresInput(modFilePath, paramsToCompare, resol = 10)
+  rawresInputList <- createRawresInput(modFilePath, paramsToCompare, resol = 10, ...)
   
   print("Running Parallel retries")
   dirName <- runParaRetries(modFilePath, rawres_input = rawresInputList[[1]], clean = 3, local = local)
