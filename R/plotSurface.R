b1a02a311569ac309836a84471a262ba523f80ce
UppsalaHenrik <henrik.b.nyberg@farmbio.uu.se>
2015-12-08 16:07:01 +0000

    Minor edits

--- R/plotSurface.R
+++ R/plotSurface.R
@@ -4,6 +4,7 @@
 #' 
 #' @param plotlyAccount A username and key for Plotly access on the form plotly(username='user', key='key').
 #' @param modFilePath Model file to use. The called function createRawresInput assumes that there is an ext file with the same base file name.
+#' @param paramsToCompare A vector of two parameter names following the NONMEM ext file standard names. Default is c("THETA1", "THETA2"). Parameters to compare. Model file cannot currently use labels for parameters.
 #' @param resol Resolution on each axis. Default is 10 and will use 10^2 = 100 sets of parameter values, NONMEM runs, and ofv values to create the plot.
 #' @param ... Further options to createRawresInput
 #' 
@@ -16,7 +17,7 @@ plotSurface <- function(plotlyAccount, modFilePath, paramsToCompare = c("THETA1"
   require(plotly)
   
   print("Creating the rawres input file")
-  rawresInputList <- createRawresInput(modFilePath, paramsToCompare, resol = 10, ...)
+  rawresInputList <- createRawresInput(modFilePath = modFilePath, paramsToCompare = paramsToCompare, resol = resol, ...)
   
   print("Running Parallel retries")
   dirName <- runParaRetries(modFilePath, rawres_input = rawresInputList[[1]], clean = 3, local = local)
