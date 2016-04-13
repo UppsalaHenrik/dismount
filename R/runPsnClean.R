0e284527a0e376b8c333b8fa0106c642ea14fb19
UppsalaHenrik <henrik.b.nyberg@farmbio.uu.se>
2016-04-13 12:02:05 +0100

    More cleaning updates

--- R/runPsnClean.R
+++ R/runPsnClean.R
@@ -1,4 +1,8 @@
+#' Run psn_clean
 #' 
+#' Runs the PsN cleanup script. Currently only the level option is mapped and wrapped.
+#' 
+#' @param level How much to clean up. Higher setting removes more. 2 - 4 is available with 3 being the default.
 #' 
 #' @export
 
