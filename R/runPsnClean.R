a00ed713ddf2bd81f0f9c9264641479f6aa01588
UppsalaHenrik <henrik.b.nyberg@farmbio.uu.se>
2016-04-19 17:47:14 +0100

    PsN cleaning improvements

--- R/runPsnClean.R
+++ R/runPsnClean.R
@@ -3,6 +3,8 @@
 #' Runs the PsN cleanup script. Currently only the level option is mapped and wrapped.
 #' 
 #' @param level How much to clean up. Higher setting removes more. 2 - 4 is available with 3 being the default.
+#' @param interact If TRUE the user is prompted for deletion or every file in an interactive session. Default is FALSE.
+#' @param addOpt String of additional options to include on command line. Must follow PsN conventions and include space in between options. Default is an empty string.
 #' 
 #' runPsnClean()
 #' 
@@ -10,9 +12,11 @@
 #' @export
 
 
-runPsnClean <- function(path = ".", level = 3){
+runPsnClean <- function(path = ".", level = 3, interact = FALSE, addOpt = ""){
   
-  cmd <- paste0("psn_clean ", path, " -level=", level)
+  ifelse(interact, interactOpt = " -interactive ", interactOpt = " -no-interactive ")
+  
+  cmd <- paste0("psn_clean ", path, " -level=", level, interactiveOpt, addOpt)
   system(cmd)
   
 }
